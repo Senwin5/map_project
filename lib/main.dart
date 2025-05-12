43605a250b11f4e8a93879ba981e5510edea2e7d
Godwin Umosen <umosengodwin568@gmail.com>
2025-05-12 16:32:55 +0100

    fouth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,45 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Material(
-      child: Text("Hello Flutter"),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[500],
+        body: Column(
+          children: [
+            Text("Hello, Welcome back"),
+            Text("Login to continue"),
+            TextField(decoration: InputDecoration(hintText: "Username")),
+            TextField(decoration: InputDecoration(hintText: "Password")),
+            TextButton(
+              onPressed: () {
+                print('Forgot is Clicked');
+              },
+              child: Text("Forgot Password"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Login is Clicked');
+              },
+              child: Text("Login "),
+            ),
+            Text("Or sign in with"),
+            ElevatedButton(
+              onPressed: () {
+                print("Google is Clicked");
+              },
+              child: Text("Login with Google"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
