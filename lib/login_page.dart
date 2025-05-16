0a8eb5fa48b557c756a467eaf1aed967e3aeae1d
Godwin Umosen <umosengodwin568@gmail.com>
2025-05-16 12:07:19 +0100

    fift commit

---
+++ lib/login_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[500],
+      body: Column(
+        children: [
+          Text(
+            "Welcome to Deus Magnus Ltd",
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 19,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 16),
+          Text("Login here to Continue",
+          style:
+          TextStyle(
+            color: Colors.white
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Username",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(12),)
+              ),
+              filled: true,
+              fillColor: Colors.white.withOpacity(0.5),
+              ),
+            ),
+          TextField(
+            decoration: InputDecoration(hintText: "Password")),
+          TextButton(
+            onPressed: () {
+              print("Forgot is clicked");
+            },
+            child: Text("Forgot is clicked"),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              print("Login is clicked");
+            },
+            child: Text("Login to Deus Magnus"),
+          ),
+          Text("Or sign up with Google"),
+          ElevatedButton(
+            onPressed: () {
+              print("Google is clicked");
+            },
+
+            child: Row(
+              children: [
+                Image.asset('assets/images/google.png', width: 22, height: 22),
+                Text("Login with Google Account"),
+              ],
+            ),
+          ),
+
+          ElevatedButton(
+            onPressed: () {
+              print("facebook is been clicked on");
+            },
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/images/facebook.png',
+                  width: 22,
+                  height: 22,
+                ),
+                Text("Login with Facebook Account"),
+              ],
+            ),
+          ),
+
+          Row(
+            children: [
+              Text("Don't have an account?"),
+              TextButton(onPressed: () {}, child: Text("Sign up here")),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:map_project/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,69 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue[500],
-        body: Column(
-          children: [
-            Text("Welcome to Deus Magnus Ltd"),
-            Text("Login here to Continue"),
-            TextField(decoration: InputDecoration(hintText: "Username")),
-            TextField(decoration: InputDecoration(hintText: "Password")),
-            TextButton(
-              onPressed: () {
-                print("Forgot is clicked");
-              },
-              child: Text("Forgot is clicked"),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                print("Login is clicked");
-              },
-              child: Text("Login to Deus Magnus"),
-            ),
-            Text("Or sign up with Google"),
-            ElevatedButton(
-              onPressed: () {
-                print("Google is clicked");
-              },
-
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/images/google.png',
-                    width: 22,
-                    height: 22,
-                  ),
-                  Text("Login with Google Account"),
-                ],
-              ),
-            ),
-
-            ElevatedButton(
-              onPressed: () {
-                print("facebook is been clicked on");
-              },
-              child: Row(
-                children: [
-                  Image.asset(
-                    'assets/images/facebook.png',
-                    width: 22,
-                    height: 22,
-                  ),
-                  Text("Login with Facebook Account"),
-                ],
-              ),
-            ),
-
-            Row(
-              children: [
-                Text("Don't have an account?"),
-                TextButton(onPressed: () {}, child: Text("Sign up here")),
-              ],
-            ),
-          ],
-        ),
-      ),
+      home: LoginPage(),
     );
   }
 }
