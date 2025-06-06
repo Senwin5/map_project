979792d3e168142b49b8570f22e195d9063d6f6c
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-06 15:33:56 +0100

    Out of Memory Debugging

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -8,18 +8,30 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor:AppColors.background,
+        backgroundColor: AppColors.background,
         title: Text('SenwinFlutter'),
         centerTitle: false,
-      actions: [
-        Icon(Icons.location_on_outlined),
-      ],
+        actions: [Icon(Icons.location_on_outlined)],
       ),
-      body: Column(
-        children: [
-          
-        ],
-      ),
+      body: Column(children: mockUsersFromServer()),
     );
+  }
+
+  Widget _userItem() {
+    return Row(
+      children: [
+        Image.asset('assets/temp/user1.png', width: 40, height: 40),
+        SizedBox(width: 16),
+        Text('Serah Foernandes'),
+      ],
+    );
+  }
+
+  List<Widget> mockUsersFromServer() {
+    List<Widget> users = [];
+    for (var i = 0; i < 20; i) {
+      users.add(_userItem());
+    }
+    return users;
   }
 }
