1b3245b8788feb1be8ef7ccfd6127bf8ba8b834a
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-13 15:46:19 +0100

    Out of Memory Debugging

--- lib/components/post_item.dart
+++ lib/components/post_item.dart
@@ -7,22 +7,27 @@ class PostItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
         
-    return Column(
-      children: [
-        Row(
-          children: [
-            Image.asset(
-              'assets/temp/user1.png',
-              width: 40, 
-              height: 40),
-            SizedBox(width: 16),
-            Text(
-              'Serah Foernandes',
-              style: AppText.subtitle3),
-          ],
-        ),
-        Image.asset('assets/temp/post.jpg')
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:24),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Image.asset(
+                'assets/temp/user1.png',
+                width: 40, 
+                height: 40),
+              SizedBox(width: 16),
+              Text(
+                'Serah Foernandes',
+                style: AppText.subtitle3),
+            ],
+          ),
+          Image.asset('assets/temp/post.jpg'),
+          Text('best free night club images. Free for use No attribution required free images from iStoi Stockck.',
+            style: AppText.subtitle3,)
+        ],
+      ),
     );
   }
 }
