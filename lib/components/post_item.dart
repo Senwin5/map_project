ed77b7a29687ea8d8db0907652a32d6b709af059
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-19 15:58:00 +0100

    static collected

--- lib/components/post_item.dart
+++ lib/components/post_item.dart
@@ -2,30 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:map_project/styles/app_text.dart';
 
 class PostItem extends StatelessWidget {
-  const PostItem({super.key});
+  final String user;
+  const PostItem({super.key, required this.user});
 
   @override
   Widget build(BuildContext context) {
-        
     return Padding(
-      padding: const EdgeInsets.symmetric(horizontal:24),
+      padding: const EdgeInsets.symmetric(horizontal: 24),
       child: Column(
         children: [
           Row(
             children: [
-              Image.asset(
-                'assets/temp/user1.png',
-                width: 40, 
-                height: 40),
+              Image.asset('assets/temp/user1.png', width: 40, height: 40),
               SizedBox(width: 16),
               Text(
-                'Serah Foernandes',
+                user,
                 style: AppText.subtitle3),
             ],
           ),
+          SizedBox(height: 16),
           Image.asset('assets/temp/post.jpg'),
-          Text('best free night club images. Free for use No attribution required free images from iStoi Stockck.',
-            style: AppText.subtitle3,)
+          Text(
+            'best free night club images. Free for use No attribution required free images from iStoi Stockck.',
+            style: AppText.subtitle3,
+          ),
         ],
       ),
     );
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,10 +3,12 @@ import 'package:map_project/components/post_item.dart';
 import 'package:map_project/styles/app_colors.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
 
+  List<String> users = [];
   @override
   Widget build(BuildContext context) {
+    mockUsersFromServer();
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.background,
@@ -14,19 +16,19 @@ class HomePage extends StatelessWidget {
         centerTitle: false,
         actions: [Icon(Icons.location_on_outlined)],
       ),
-      body: ListView(children: mockUsersFromServer()),
+      body: ListView.builder(itemBuilder: (context, index) {
+        return PostItem( user:users[index],);
+      },),
     );
   }
 
   // adding a method call _userItem for the list of user and profile
 
-
   // creating list of widget e'g mockUsersFromServer from server
-  List<Widget> mockUsersFromServer() {
-    List<Widget> users = [];
+  mockUsersFromServer() {
+    //List<Widget> users = [];
     for (var i = 0; i < 400; i++) {
-      users.add(PostItem());
+      users.add('User number $i');
     }
-    return users;
   }
 }
